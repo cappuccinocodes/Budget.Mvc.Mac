ac3910ec679a5269c5b8861a9f4c8069d24120c5
pablo-the-souza <53923086+pablo-the-souza@users.noreply.github.com>
2022-05-07 07:46:17 +1000

    Button plus modal

--- Budget.Mvc.Mac/wwwroot/js/site.js
+++ Budget.Mvc.Mac/wwwroot/js/site.js
@@ -1,5 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
+﻿$("#openTransactionModalBtn").on("click", function () {
+    console.log('t modal');
+    $("#addTransactionModal").modal("show");
+});
